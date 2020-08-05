fe9c91e7df6961ec4d784f07c4f43fe5e658fd1f
AkiJoey <akijoey1010635951@gmail.com>
2020-08-05 21:26:17 +0800

    feat: remove static

--- src/main/webapp/components/footer.tsx
+++ src/main/webapp/components/footer.tsx
@@ -5,7 +5,7 @@ const Footer = (): JSX.Element => {
 	return (
 		<div id="footer">
 			<div id="copyright">©2020 By <a href="https://akijoey.com">AkiJoey</a></div>
-			<div id="framework">Power by <a href="https://reactjs.org">React</a></div>
+			<div id="framework">Power by <a href="https://spring.io">Spring</a> + <a href="https://reactjs.org">React</a></div>
 		</div>
 	);
 }
